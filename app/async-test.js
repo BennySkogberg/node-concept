5494dcb1455a544a5ee91e26a93f8c8c9b5e7358
Benny Skogberg <benny.skogberg@gmail.com>
2017-10-17 13:49:47 +0200

    added async test

---
+++ app/async-test.js
@@ -0,0 +1,19 @@
+/**
+ * Execute by running
+ * 'npm run async'
+ * according to package.json
+ */
+
+const fs = require('fs')
+
+console.log('start reading the file...')
+
+fs.readFile('app/file.md', 'utf-8', function (err, content) {
+  if (err) {
+    console.log('error happened during reading the file')
+    return console.log(err)
+  }
+  console.log(content)
+})
+
+console.log('end of the file')
