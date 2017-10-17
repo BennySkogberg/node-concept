bd48611424164c3cc2018b981e3b60a54a32d8b7
Benny Skogberg <benny.skogberg@gmail.com>
2017-10-17 10:20:23 +0200

    editing the index .js to see changes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 /**
  * From the Blog "Node Hero - Getting Started With Node.js Tutorial"
  * https://blog.risingstack.com/node-hero-tutorial-getting-started-with-node-js/
+ *
+ * Author: Benny Skogberg
  */
 
 require('./app/index')
